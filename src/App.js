a8307cdaa8e0fe7e7f0f8ece5d12539f29d02307
ZhangaliMalikazhdar <spectrumus123@gmail.com>
2023-04-14 01:15:46 +0600

    children props

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './styles/App.css';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -9,16 +10,14 @@ function App() {
     {id: 3, title: 'Javascript 3', body: 'desc'},
   ])
 
-  const [posts2, setPosts2] = useState([
-    {id: 1, title: 'Python 1', body: 'desc'},
-    {id: 2, title: 'Python 2', body: 'desc'},
-    {id: 3, title: 'Python 3', body: 'desc'},
-  ])
-
   return (
     <div className="App">
+      <form>
+        <input type='text' placeholder='Name of post'/>
+        <input type='text' placeholder='desc of post'/>
+        <MyButton>Create post</MyButton>
+      </form>
       <PostList posts={posts} title='List of posts 1' />
-      <PostList posts={posts2} title='List of posts 2' />
     </div>
   );
 }
