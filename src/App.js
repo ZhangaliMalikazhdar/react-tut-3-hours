1d75749f254cfebc26f329c2877cf6fc934a63b2
ZhangaliMalikazhdar <spectrumus123@gmail.com>
2023-04-14 00:56:50 +0600

    some const post names

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React, { useState } from 'react';
-import Counter from './components/Counter';
-import ClassCounter from './components/ClassCounter';
 import './styles/App.css';
-import PostItem from './components/PostItem';
+import PostList from './components/PostList';
 
 function App() {
+  const [posts, setPosts] = useState([
+    {id: 1, title: 'Javascript 1', body: 'desc'},
+    {id: 2, title: 'Javascript 2', body: 'desc'},
+    {id: 3, title: 'Javascript 3', body: 'desc'},
+  ])
+
+  const [posts2, setPosts2] = useState([
+    {id: 1, title: 'Python 1', body: 'desc'},
+    {id: 2, title: 'Python 2', body: 'desc'},
+    {id: 3, title: 'Python 3', body: 'desc'},
+  ])
+
   return (
     <div className="App">
-      <PostItem post={{id: 1, title: 'Javascript', body: 'Description'}} />
-      {/* <ClassCounter />
-      <Counter /> */}
+      <PostList posts={posts} title='List of posts 1' />
+      <PostList posts={posts2} title='List of posts 2' />
     </div>
   );
 }
