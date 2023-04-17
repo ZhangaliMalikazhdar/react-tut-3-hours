3aeaa7849a0d54fa0b5f1077c85cd47337cb078c
ZhangaliMalikazhdar <spectrumus123@gmail.com>
2023-04-18 02:01:33 +0600

    sort

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import PostList from './components/PostList';
 import MyButton from './components/UI/button/MyButton';
 import MyInput from './components/UI/input/MyInput';
 import PostForm from './components/PostForm';
+import MySelect from './components/UI/select/MySelect';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -12,14 +13,44 @@ function App() {
     {id: 3, title: 'Javascript 3', body: 'desc'},
   ])
 
+  const [selectedSort, setSelectedSort] = useState('')
+
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
+  }
+
+  const removePost = (post) => {
+    setPosts(posts.filter(p => p.id != post.id))
+  }
+
+  const sortPosts = (sort) => {
+    setSelectedSort(sort);
+    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
   }
 
   return (
     <div className="App">
       <PostForm create={createPost}/>
-      <PostList posts={posts} title='List of posts 1' />
+      <hr style={{margin: '15px 0'}}/>
+      <div>
+        <MySelect 
+          value={selectedSort}
+          onChange={sortPosts}
+          defaultValue="Sort"
+          options={[
+            {value: 'title', name: 'by name'},
+            {value: 'body', name: 'by description'},
+          ]}
+        />
+      </div>
+      {posts.length
+        ? 
+        <PostList remove={removePost} posts={posts} title='List of posts 1' />
+        : 
+        <h1 style={{textAlign: 'center'}}>
+          posts is empty
+        </h1>
+      }
     </div>
   );
 }
