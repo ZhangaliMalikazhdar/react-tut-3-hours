9226e15f25a623dab99d3bccfee9c2f63e8d0922
ZhangaliMalikazhdar <spectrumus123@gmail.com>
2023-04-19 01:39:37 +0600

    sort work after every word in search input

--- src/App.js
+++ src/App.js
@@ -8,12 +8,22 @@ import MySelect from './components/UI/select/MySelect';
 
 function App() {
   const [posts, setPosts] = useState([
-    {id: 1, title: 'Javascript 1', body: 'desc'},
-    {id: 2, title: 'Javascript 2', body: 'desc'},
-    {id: 3, title: 'Javascript 3', body: 'desc'},
+    {id: 1, title: 'Javascript 1', body: 'b'},
+    {id: 2, title: 'Javascript 2', body: 'a'},
+    {id: 3, title: 'Javascript 3', body: 'c'},
   ])
 
   const [selectedSort, setSelectedSort] = useState('')
+  const [searchQuery, setSearchQuery] = useState('')
+
+  function getSortedPosts() {
+    if(selectedSort) {
+      console.log('sorted')
+      return [...posts].sort((a, b) => a[selectedSort].localeCompare(b[selectedSort]))
+    }
+    return posts;
+  }
+  const sortedPosts = getSortedPosts()
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -25,7 +35,6 @@ function App() {
 
   const sortPosts = (sort) => {
     setSelectedSort(sort);
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])))
   }
 
   return (
@@ -33,6 +42,11 @@ function App() {
       <PostForm create={createPost}/>
       <hr style={{margin: '15px 0'}}/>
       <div>
+        <MyInput
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+          placeholder="search..."
+        />
         <MySelect 
           value={selectedSort}
           onChange={sortPosts}
@@ -45,7 +59,7 @@ function App() {
       </div>
       {posts.length
         ? 
-        <PostList remove={removePost} posts={posts} title='List of posts 1' />
+        <PostList remove={removePost} posts={sortedPosts} title='List of posts 1' />
         : 
         <h1 style={{textAlign: 'center'}}>
           posts is empty
