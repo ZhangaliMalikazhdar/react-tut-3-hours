e27db739d1199396aff4114abb27c652587bf82a
ZhangaliMalikazhdar <spectrumus123@gmail.com>
2023-04-24 20:00:12 +0600

    2nd method pagination correct id numbering

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ function App() {
   const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query);
   let pagesArray = getPagesArray(totalPages);
 
-  const [fetchPosts, isPostsLoading, postError] = useFetching(async () => {
+  const [fetchPosts, isPostsLoading, postError] = useFetching(async (limit, page) => {
     const response = await PostService.getAll(limit, page);
     setPosts(response.data)
     const totalCount = response.headers['x-total-count']
@@ -32,8 +32,8 @@ function App() {
   })
 
   useEffect(() => {
-    fetchPosts()
-  }, [page])
+    fetchPosts(limit, page)
+  }, [])
 
   const createPost = (newPost) => {
     setPosts([...posts, newPost])
@@ -46,7 +46,7 @@ function App() {
 
   const changePage = (page) => {
     setPage(page)
-    fetchPosts(page)
+    fetchPosts(limit, page)
   }
 
   return (
