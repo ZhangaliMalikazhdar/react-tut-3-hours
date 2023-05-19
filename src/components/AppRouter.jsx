4ba8e810ff493e98c487fbfdc2956fe7febaa35e
ZhangaliMalikazhdar <spectrumus123@gmail.com>
2023-05-19 13:52:47 +0600

    id page

--- src/components/AppRouter.jsx
+++ src/components/AppRouter.jsx
@@ -3,12 +3,14 @@ import { BrowserRouter, Routes, Route, Link, Switch, Redirect, Navigate } from '
 import About from '../pages/About';
 import Posts from '../pages/Posts';
 import Error from '../pages/Error';
+import PostIdPage from '../pages/PostIdPage';
 
 const AppRouter = () => {
     return (
         <Routes>
             <Route path="/about" element={<About />} />
-            <Route path="/posts" element={<Posts />} />
+            <Route exact path="/posts" element={<Posts />} />
+            <Route exact path="/posts/:id" element={<PostIdPage />} />
             {/* <Route element={<Redirect to='/posts' />} /> */}
             <Route path='/error' element={<Error />}/>
             <Route path='*' element={<Navigate to='/error' replace />} />
---
+++ src/pages/PostIdPage.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PostIdPage = () => {
+    return (
+        <div>
+            <h1>post id page</h1>
+        </div>
+    );
+};
+
+export default PostIdPage;
